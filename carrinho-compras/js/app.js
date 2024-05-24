d924737dddc31db21fa66b7b410fce05ea4d5ca5
Leonardo Almeida <leonardo.pereira204@etec.sp.gov.br>
2024-05-24 14:49:15 -0300

    Adicionando funcionalidade no carrinho de compras, carrinho de compra dinamico

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,6 +1,9 @@
+var getProdutosCarrinho = document.getElementsByClassName('carrinho__produtos__produto')
 let getProdutoSelecionado = document.getElementById('produto');
 let getInputQuantidade = document.getElementById('quantidade');
 let tagValorTotal = document.getElementById('valor-total');
+let carinhoAtual = [];
+
 function adicionar() {
     quantidadeProduto = parseInt(getInputQuantidade.value);
     valorProduto = parseInt(getProdutoSelecionado.value.split('-')[1].substring(3));
@@ -8,14 +11,24 @@ function adicionar() {
 
     if (isNaN(quantidadeProduto)) return;
 
-    carinho = document.getElementById('lista-produtos');
-    carinho.innerHTML += `<section class="carrinho__produtos__produto"><span class="texto-azul">${quantidadeProduto}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span></section>`;
-    
+    atualizarCarrinho(nomeProduto, quantidadeProduto, valorProduto);
     atualizarValor(valorProduto, quantidadeProduto);
 }
 
-function atualizarValor(valorProduto, quantidadeProduto) {
 
+function atualizarCarrinho(nomeProduto, quantidadeProduto, valorProduto) {
+    verificaSePossuiProdutoRepetido(nomeProduto, quantidadeProduto, valorProduto);
+    limpaCarrinho();
+
+    carinhoAtual.forEach(function(produto) {
+        document.getElementById('lista-produtos').innerHTML += 
+        `<section class="carrinho__produtos__produto">
+        <span class="texto-azul">${produto.quantidade}x</span> ${produto.nome} <span class="texto-azul">R$${produto.valor}</span>
+        </section>`;
+    });
+}
+
+function atualizarValor(valorProduto, quantidadeProduto) {
     if (tagValorTotal.textContent == '') {
         valorAtual = 0;
     } else {
@@ -28,10 +41,32 @@ function atualizarValor(valorProduto, quantidadeProduto) {
 }
 
 function limpar() {
-    var elementos = document.getElementsByClassName('carrinho__produtos__produto');
-    var pai = elementos[0].parentNode;
-    pai.innerHTML = '';
+    limpaCarrinho();
 
     tagValorTotal.textContent = 'R$0';
     getInputQuantidade.value = '';
+    carinhoAtual = [];
+}
+
+function verificaSePossuiProdutoRepetido(nomeProduto, quantidadeProduto, valorProduto) {
+    let novoProduto = {
+        nome: nomeProduto,
+        quantidade: quantidadeProduto,
+        valor: valorProduto
+    };
+
+    for (let i = 0; i < carinhoAtual.length; i++) {
+        if (carinhoAtual[i].nome == nomeProduto && carinhoAtual[i].valor == valorProduto) {
+            carinhoAtual[i].quantidade += quantidadeProduto;
+            return;
+        }
+    }
+    
+    carinhoAtual.push(novoProduto);
+}
+
+function limpaCarrinho() {
+    var elementos = getProdutosCarrinho ? getProdutosCarrinho : '';
+    var pai = elementos[0] ? elementos[0].parentNode : '';
+    pai.innerHTML = '';
 }
